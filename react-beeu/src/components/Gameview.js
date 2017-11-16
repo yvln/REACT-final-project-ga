1c76b8e8a6ba7a5277cb7181c89be1b53a7e8314
yvln <sayyveline@gmail.com>
2017-11-16 11:28:52 -0500

    games 1 to 4 works, manage lose/win

--- react-beeu/src/components/Gameview.js
+++ react-beeu/src/components/Gameview.js
@@ -8,12 +8,13 @@ class Gameview extends Component {
     
     this.state = {
       // game page
-      rungame: false,
-      counter: 30,
+      rungame: 'before',
+      counter: 5,
       
       // user info
       tryleft: null,
       last_try: null,
+      max_score: null,
 
       // game info
       numberErrorAllowed: null,
@@ -23,6 +24,7 @@ class Gameview extends Component {
       // user info on the game
       score: 0,
       error: 0,
+      wins: null,
       
       // game each question
       question: '',
@@ -30,89 +32,151 @@ class Gameview extends Component {
       response: ''
     }
     
+    this.initializeState = this.initializeState.bind(this);
+    
     this.getGameInfo = this.getGameInfo.bind(this);
     this.renderGame = this.renderGame.bind(this);
     
     this.decreaseCounter = this.decreaseCounter.bind(this);
+    this.checkCounter = this.checkCounter.bind(this);
     this.setCounter = this.setCounter.bind(this);
     
     this.finishGame = this.finishGame.bind(this);
-    this.counterUntilTomorrow = this.counterUntilTomorrow.bind(this);
+    // this.counterUntilTomorrow = this.counterUntilTomorrow.bind(this);
     
     this.renderQuestion = this.renderQuestion.bind(this);
     this.displayQuestion = this.displayQuestion.bind(this);
     this.checkAnswer = this.checkAnswer.bind(this);
+    this.gameChecker = this.gameChecker.bind(this);
   }
   
   componentDidMount() {
-    this.setState({
-      tryleft: this.props.user.number_try_game,
-      last_try: this.props.user.last_try
-    });
+    this.initializeState();
     this.getGameInfo();
   }
   
-  getGameInfo() {
-    axios.get(`${this.props.url}/games/${this.props.whichGame.id}/${this.props.user.id}`)
-    .then( res => {
-      console.log(res.data);
-      // this.setState({
-      //   numberErrorAllowed: res.data.game.nb_try_max_level_this.props.user.level,
-      //   scoreToReach: res.data.game.points_to_reach_level_this.props.user.level,
-      //   hint: res.data.game.hint
-      // })
+  initializeState() {
+    this.setState({
+      // game page
+      counter: 5,
+      
+      // user info
+      tryleft: this.props.user.number_try_game,
+      last_try: this.props.user.last_try,
+      max_score: this.props.user[`max_score_game_${this.props.whichGame.id}`],
+
+      // game info
+      numberErrorAllowed: null,
+      scoreToReach: null,
+      hint: '',
+      
+      // user info on the game
+      score: 0,
+      error: 0,
+      wins: null,
+      
+      // game each question
+      question: '',
+      choice: [],
+      response: ''
     })
-    // get number error OK : state numberErrorAllowed
-    // get score to reach function of level : scoreToReach
-    // add it on line 121
+  }
+  
+  getGameInfo() {
+    this.setState({
+      numberErrorAllowed: this.props.whichGame[`nb_try_max_level_${this.props.user.level}`],
+      scoreToReach: this.props.whichGame[`points_to_reach_level_${this.props.user.level}`],
+      hint: this.props.whichGame[`hint`]
+    })
   }
   
   renderGame() {
+    this.initializeState();
+    this.getGameInfo();
     this.setState({
-      rungame: true
+      rungame: 'during'
     }),
-    this.decreaseCounter();
+    axios.post(`${this.props.url}/games/updateLastPlay`, {
+      user_id: this.props.user.id,
+      last_try: new Date()
+    }).then( res => {
+      console.log('res');
+    })
+    this.setCounter();
     this.renderQuestion();
+    this.gameChecker();
   }
   
   decreaseCounter() {
+    console.log('IN DECREASE COUNTER');
     this.setState({
       counter: this.state.counter - 1
-    })
+    }),
+    this.checkCounter();
+  }
+  
+  checkCounter() {
+    console.log('IN CHECK COUNTER');
+    if (this.state.counter === 0) {
+      console.log('COUNTER = 0!');
+      this.finishGame('lose');
+    }
   }
   
   setCounter() {
-    setInterval(this.decreaseCounter, 1000);
-    if (this.state.counter === 0) {
-      this.finishGame();
+    this.countdown = setInterval(this.decreaseCounter, 1000);
+  }
+  
+  finishGame(WinOrLose) {
+    console.log('WIN OR LOSE?', WinOrLose);
+    clearInterval(this.countdown);
+    this.setState({
+      rungame: 'after'
+    });
+  
+    if (WinOrLose === 'lose') {
+      console.log('LOSE???? YES!');
+      this.setState({
+        wins: false
+      })
+      axios.post(`${this.props.url}/games/updateNumberTry`, {
+        new_nb_try: this.props.user.number_try_game - 1,
+        user_id: this.props.user.id
+      }).then( res => {
+        console.log('IS NUMBER TRY UPDATED? res.data.new_nb_try:', res.data.new_nb_try);
+        this.setState({
+          tryleft: res.data.new_nb_try
+        })
+      })
+    } else if (WinOrLose === 'win') {
+      this.setState({
+        wins: true
+      })
+      if (this.state.score > this.state.max_score) {
+        axios.post(`${this.props.url}/games/updateMaxScore`, {
+          user_id: this.props.user.id,
+          score: this.state.score,
+          game_id: this.props.whichGame.id
+        }).then( res => {
+          this.setState({
+            max_score: res.data.new_max_score
+          })
+        })
+      }
     }
   }
   
-  finishGame() {
-    console.log('counter finished');
-    clearInterval(this.decreaseCounter);
-    // axios.post(`${this.props.url}/games/lessTry`, {
-    //   number_try_game: this.props.user.number_try_game - 1,
-    //   user_id: this.props.user.id
-    // }).then( res => {
-      // this.setState({
-      //   tryleft: this.props.user.number_try_game
-      // })
-    //   
-    //   })
-    // })
+  // counterUntilTomorrow() {
+  //   console.log('IN COUNTER UNTIL TOMORROW');
+  //   if (this.state.last_try.substring(0,10) === new Date().substring(0,10)) {
+  //     console.log('getHours', new Date().getHours());
+  //     console.log('getMinutes', new Date().getMinutes());
+  //     console.log('getSeconds', new Date().getSeconds());
+  //   }
   // }
-  }
-  
-  counterUntilTomorrow() {
-    if (this.state.last_try.substring(0,10) === new Date().substring(0,10)) {
-      console.log('getHours', new Date().getHours());
-      console.log('getMinutes', new Date().getMinutes());
-      console.log('getSeconds', new Date().getSeconds());
-    }
-  }
   
   renderQuestion() {
+    console.log('IN RENDER QUESTION');
     axios.get(`${this.props.url}/games/renderQuestion/${this.props.whichGame.id}/${this.props.user.id}`)
     .then( data => {
       this.setState({
@@ -124,6 +188,7 @@ class Gameview extends Component {
   }
   
   displayQuestion() {
+    console.log('IN displayQuestion');
     return (
       <div>
         <div className="question">{this.state.question}</div>
@@ -146,6 +211,7 @@ class Gameview extends Component {
   }
   
   checkAnswer(answerClicked) {
+    console.log('iN checkAnswer');
     if (answerClicked === this.state.response) {
       // add condition if score reached
       this.setState({
@@ -156,41 +222,64 @@ class Gameview extends Component {
       this.setState({
         error: this.state.error + 1
       })
-    } else if (answerClicked !== this.state.response) {
-      this.renderQuestion();
+    }
+    this.renderQuestion();
+  }
+  
+  gameChecker() {
+    console.log('iN GAME CHECKER');
+    if (this.state.error === this.state.numberErrorAllowed) {
+      this.finishGame('lose');
+    } else if (this.state.score === this.state.scoreToReach ) {
+      this.finishGame('win');
     }
   }
   
-
   render() {
-    // console.log(this.state.numberErrorAllowed);
-    // console.log(this.state.scoreToReach);
-    // console.log(this.state.hint);
+    console.log('iN GAME VIEW');
     return (
       <div className="Gameview">
           <div className="GameContainer">
-            { ((this.props.user.number_try_game >= 0) && (!this.state.rungame)) &&
-              <div>
+            { ((this.props.user.number_try_game > 0) && (this.state.rungame === 'before')) &&
+              <div className="beforePlaying">
                 {this.props.whichGame.name}
                 You have 30 seconds to {this.props.whichGame.rules}
                 Ready ?
                 <button onClick={this.renderGame}>GO</button>
               </div>
             }
-            { ((this.props.user.number_try_game >= 0) && (this.state.rungame)) &&
+            { ((this.props.user.number_try_game > 0) && (this.state.rungame === 'during')) &&
               <div className="gameBlock">
                 <div className="upQuestionBlock">
-                  <div className="error">{this.state.error} / {this.state.numberErrorAllowed}</div>
-                  <div className="score">{this.state.score} / {this.state.scoreToReach}</div>
-                  <div className="counter">{this.state.counter}</div>
+                  <div className="error">Chances: {this.state.error} / {this.state.numberErrorAllowed}</div>
+                  <div className="score">Score: {this.state.score} / {this.state.scoreToReach}</div>
+                  {this.state.hint !== 'x' &&
+                    <div className="score">Hint: {this.state.hint}</div>
+                  }
+                  <div className="counter">00:{this.state.counter}</div>
                 </div>
                 <div className="fullQuestionBlock">{this.displayQuestion()}</div>
               </div>
             }
-            { this.props.user.number_try_game < 0 &&
+            { ((this.props.user.number_try_game > 0) && (this.state.rungame === 'after')) &&
+              <div>
+                {this.state.wins &&
+                  <div className="afterPlaying wins">
+                      Want to play to something else?
+                  </div>
+                }
+                {!this.state.wins &&
+                  <div className="afterPlaying loses">
+                      Want to try again?
+                      <button onClick={this.renderGame}>GO</button>
+                  </div>
+                }
+              </div>
+            }
+            { this.props.user.number_try_game <= 0 &&
               <div>
                 Sorry... you tried enough for today!
-                {this.counterUntilTomorrow()}
+                Please wait 
               </div>
             }
           </div>
